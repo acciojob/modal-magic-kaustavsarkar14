48db4202da36b2e5a3389a29527236c6cca136a8
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-10-05 11:32:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const modal = document.getElementById('modal')
+const open = document.getElementById('open')
+function handleClick(){
+	modal.style.display = "block"
+}
+function handleClose(){
+	modal.style.display = "none"
+}
+document.addEventListener('click',(e)=>{
+	if(e.target !== open && !modal.contains(e.target)){
+	handleClose()
+	}
+})
